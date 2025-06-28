1d7e3ca82e5df3273c7407ed0b876de171f3ebfb
Rutger Pronk <rutger.pronk11@gmail.com>
2025-06-28 23:24:17 +0200

    Remove template related things and start creating Todos

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { auth, signIn, signOut } from "@/server/auth";
+
+export async function Header() {
+  const session = await auth();
+
+  return (
+    <header className="flex justify-between p-10">
+      <span>Todo&#39;s</span>
+
+      {session ? (
+        <div className={"flex gap-5"}>
+          <span>{session.user.name}</span>
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+          >
+            <Button type="submit">Sign Out</Button>
+          </form>
+        </div>
+      ) : (
+        <form
+          action={async () => {
+            "use server";
+            await signIn();
+          }}
+        >
+          <Button type="submit">Sign In</Button>
+        </form>
+      )}
+    </header>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,16 @@
-import { PostCreate } from "@/app/_components/post-create";
-import { PostList } from "@/app/_components/post-list";
-import { auth, signIn, signOut } from "@/server/auth";
+import { Header } from "@/app/_components/Header";
 
 export default async function Home() {
-  const session = await auth();
-
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <div className="min-h-screen">
+      <Header />
+
       <main className={"flex flex-col items-center justify-center gap-10"}>
         <div className={"flex flex-col items-center space-y-2.5"}>
-          <h1 className="text-4xl font-bold">Hi There!</h1>
-          <h2 className={"text-1xl max-w-md text-center font-bold"}>
-            This is a demo blog app with database integration and discord
-            messages for showing usage of these tools
-          </h2>
+          <h1 className="text-4xl font-bold">Todo&#39;s!</h1>
         </div>
-        {session ? (
-          <div className={"flex flex-col items-center space-y-2.5"}>
-            <span className={"text-center"}>
-              Session: {JSON.stringify(session)}
-            </span>
-            <form
-              action={async () => {
-                "use server";
-                await signOut();
-              }}
-            >
-              <button type="submit">Sign Out</button>
-            </form>
-          </div>
-        ) : (
-          <form
-            action={async () => {
-              "use server";
-              await signIn();
-            }}
-          >
-            <button type="submit">Sign In</button>
-          </form>
-        )}
 
-        <div className={"flex flex-col gap-7"}>
-          {session && <PostCreate />}
-
-          <PostList session={session} />
-        </div>
+        <div className={"flex flex-col gap-7"}>No todo&#39;s yet</div>
       </main>
     </div>
   );
