2a9c18c601e5167a59ed82c8ce6391beb46ea946
Rutger Pronk <rutger.pronk11@gmail.com>
2025-07-01 19:51:49 +0200

    Make delete work

--- src/app/_components/Todo.tsx
+++ src/app/_components/Todo.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { deleteTodo } from "@/app/todosActions";
 import { type Todo } from "@/server/db/schema";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { clsx } from "clsx";
 import { Trash2 } from "lucide-react";
 
 interface Props {
@@ -8,6 +11,17 @@ interface Props {
 }
 
 export function Todo({ todo }: Readonly<Props>) {
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: deleteTodo,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ["todos"],
+      });
+    },
+  });
+
   return (
     <div
       className={
@@ -16,9 +30,11 @@ export function Todo({ todo }: Readonly<Props>) {
     >
       <span className={"py-2 pl-2 text-xl"}>{todo.name}</span>
       <button
-        className={
-          "flex aspect-square h-fit w-fit items-center justify-center rounded-full p-2 hover:bg-red-100"
-        }
+        onClick={() => deleteMutation.mutate({ id: todo.id })}
+        className={clsx(
+          "flex aspect-square h-fit w-fit items-center justify-center rounded-full p-2 hover:bg-red-100",
+          deleteMutation.isPending && "cursor-not-allowed opacity-50",
+        )}
       >
         <Trash2 size={16} />
       </button>
