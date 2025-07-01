5844a7b5ff50dbf87cf3713c61a7d619bb13a5cb
Rutger Pronk <rutger.pronk11@gmail.com>
2025-07-01 23:38:54 +0200

    Add devtools and start updating todo

--- src/app/_components/providers/QueryProvider.tsx
+++ src/app/_components/providers/QueryProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { type ReactNode } from "react";
 
 const queryClient = new QueryClient();
@@ -11,6 +12,9 @@ export default function QueryProvider({
   children: ReactNode;
 }>) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
