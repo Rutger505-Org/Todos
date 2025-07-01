ca6874bf5dcc8aea9ccb169deab8dd498efd9526
Rutger Pronk <rutger.pronk11@gmail.com>
2025-07-01 19:19:31 +0200

    Only do middleware on normal pages and server actions

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,23 @@
 import { env } from "@/env";
+import { type NextRequest } from "next/server";
 
-export async function middleware() {
+export async function middleware(request: NextRequest) {
   if (env.NODE_ENV === "development") {
-    // delay 150ms - 300ms
     const delay = Math.floor(Math.random() * 150) + 150;
     console.log(`Artificial delay of ${delay}ms`);
     await new Promise((resolve) => setTimeout(resolve, delay));
   }
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
