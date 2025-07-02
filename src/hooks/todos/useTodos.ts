1e8787eefa088a8dad37fea020ecfab81e983d67
Rutger Pronk <rutger.pronk11@gmail.com>
2025-07-02 23:56:25 +0200

    Add more fields for todos

--- src/hooks/todos/useTodos.ts
+++ src/hooks/todos/useTodos.ts
@@ -3,8 +3,16 @@ import { todoKeys } from "@/hooks/todos/todoKeys";
 import { useQuery } from "@tanstack/react-query";
 
 export function useTodos() {
-  return useQuery({
+  const query = useQuery({
     queryKey: todoKeys.all,
     queryFn: getTodos,
   });
+
+  return {
+    data: query.data,
+    isPending: query.isPending,
+    error: query.error,
+    uncompletedTodos: query.data?.filter((todo) => !todo.completed),
+    completedTodos: query.data?.filter((todo) => todo.completed),
+  };
 }
