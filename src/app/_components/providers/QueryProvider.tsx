a34be0b4b302ad21a334ae8c2d3aa53c960e9f71
Rutger Pronk <rutger.pronk11@gmail.com>
2025-07-06 18:35:55 +0200

    Actually create global error handeler

--- src/app/_components/providers/QueryProvider.tsx
+++ src/app/_components/providers/QueryProvider.tsx
@@ -1,10 +1,23 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Logging } from "@/app/util/logging";
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { type ReactNode } from "react";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => Logging.error(`Something went wrong: ${error.message}`),
+  }),
+  mutationCache: new MutationCache({
+    onError: (error) => Logging.error(`Something went wrong: ${error.message}`),
+  }),
+});
 
 export default function QueryProvider({
   children,
